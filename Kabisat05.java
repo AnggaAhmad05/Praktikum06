55616897e03db4323ad6d6b23dc2c0d04046ba9c
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-10-11 11:52:56 +0700

    modifikasi percobaan 1

---
+++ Kabisat05.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Kabisat05 {
+    public static void main(String[] args) {
+    Scanner input05 = new Scanner (System.in);
+
+    int tahun;
+    System.out.println("Masukan Tahun");
+    tahun=input05.nextInt();
+
+    if ((tahun % 4) == 0 && (tahun % 100) != 0) {
+        System.out.println("tahun Kabisat");
+
+    }else
+        System.out.println("Bukan tahun kabisat");
+
+
+}
+    
+}
